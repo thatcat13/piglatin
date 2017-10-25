4c55fdb4acd288a2c10db5a098767ecd21dfddce
Cat Janowitz and John Sawiris <cat@>
2017-10-25 16:05:06 -0700

    add logic that works for qu words but not squ words yet

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@
 var toPigLatin = function(sentence) {
   var vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
   var splitSentence = sentence.split("");
+  // var qu = "qu";
+  // var squ = "squ";
+
 
   if(vowels.includes(sentence.charAt(0))){
     return sentence += "way";
@@ -9,7 +12,22 @@ var toPigLatin = function(sentence) {
     for(var i = 0; i < sentence.length; i++) {
       if(!vowels.includes(sentence.charAt(i))) {
         splitSentence.push(splitSentence.shift());
-      } else {
+        console.log(splitSentence);
+        
+        //This is the code that tests "Q" and "U"
+        if ((sentence.charAt(0) === "s") && (sentence.charAt(1) === "q") && (sentence.charAt(2) === "u")) {
+          console.log(splitSentence);
+          splitSentence.push(splitSentence.shift());
+          console.log(splitSentence);
+        }
+        else if ((sentence.charAt(0) === "q") && (sentence.charAt(1) === "u")) {
+          console.log(splitSentence);
+          splitSentence.push(splitSentence.shift());
+          console.log(splitSentence);
+        }
+
+      }
+      else {
         splitSentence.push("ay");
         return splitSentence.join("");
       }
